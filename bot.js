7537667eca9c6000e7c4a3eed08a5d012321bb58
Nir.Dahan01 <nir.dahan01@post.runi.ac.il>
2026-03-22 18:38:15 +0200

    feat: startup notification now includes full developer status

--- bot.js
+++ bot.js
@@ -337,8 +337,9 @@ function handleStartupNotification(client) {
     setTimeout(async () => {
         try {
             const restartData = getRestartReason();
-            const msg = formatRestartMessage(VERSION, restartData);
-            await client.sendMessage(DEVELOPER_JID, msg);
+            const restartLine = formatRestartMessage(VERSION, restartData);
+            const statusMsg = await handlers.buildStatusMessage(client);
+            await client.sendMessage(DEVELOPER_JID, restartLine + '\n\n' + statusMsg);
         } catch (e) {
             logger.error('Failed to send startup notification', e);
         }
